988d5a35b2e1779537fe5b936211132989e49357
João Fernandes <joaovitorfernandes2@gmail.com>
2018-06-22 00:14:13 -0300

    Use signal generator to control the regressive counter

--- bomba.vhd
+++ bomba.vhd
@@ -8,6 +8,7 @@ ENTITY bomba IS
 		contagem_ativa: IN STD_LOGIC;
 		codigo_desarme: IN STD_LOGIC_VECTOR(3 DOWNTO 0);
 		clock: IN STD_LOGIC;
+		control: IN STD_LOGIC_VECTOR(1 DOWNTO 0);
 		
 		display_minutos: OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
 		display_horas: OUT STD_LOGIC_VECTOR(6 DOWNTO 0);
@@ -18,29 +19,41 @@ END bomba;
 
 ARCHITECTURE behavior OF bomba IS
 
-SIGNAL clock_minutos: STD_LOGIC;
+SIGNAL clock_segundos, clock_minutos, qwer: STD_LOGIC;
 SIGNAL segundos_buffer: STD_LOGIC_VECTOR(5 DOWNTO 0);
+
+COMPONENT signal_generator
+	PORT(
+		clock: IN STD_LOGIC;
+		controle: IN STD_LOGIC_VECTOR(1 DOWNTO 0);
+		q: OUT STD_LOGIC
+	);
+END COMPONENT;
 
 COMPONENT regressive_counter
 	PORT (
 		clock: IN STD_LOGIC;
+		
 		q: OUT STD_LOGIC_VECTOR(5 DOWNTO 0)
 	);
 END COMPONENT;
-	
-BEGIN	
-	segundos: regressive_counter
-		PORT MAP (clock => clock, q => segundos_buffer);
-	
-	clock_minutos <= segundos_buffer(5) AND
-						  segundos_buffer(4) AND
-						  segundos_buffer(3) AND
-						  NOT segundos_buffer(2) AND
-						  segundos_buffer(1) AND
-						  segundos_buffer(0);
-	
-	minutos: regressive_counter
-		PORT MAP (clock => clock_minutos, q => contagem_minutos);
-		
-	contagem_segundos <= segundos_buffer;
+
+BEGIN
+	gerador_de_sinal: signal_generator
+		PORT MAP (clock => clock, controle => control, q => clock_segundos);
+
+	segundos: regressive_counter 
+		PORT MAP (clock => clock_segundos, q => segundos_buffer); 
+   
+	clock_minutos <= segundos_buffer(5) AND 
+						  segundos_buffer(4) AND 
+						  segundos_buffer(3) AND 
+						  NOT segundos_buffer(2) AND 
+                    segundos_buffer(1) AND 
+						  segundos_buffer(0); 
+
+	minutos: regressive_counter 
+		PORT MAP (clock => clock_minutos, q => contagem_minutos); 
+
+  contagem_segundos <= segundos_buffer; 
 END behavior;
