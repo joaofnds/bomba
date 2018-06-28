8aab21c5d216a81b96163dda9590a4b5e8544d3e
João Fernandes <joaovitorfernandes2@gmail.com>
2018-06-27 23:10:19 -0300

    Add integer register

---
+++ integer_register.vhd
@@ -0,0 +1,21 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.all;
+
+ENTITY integer_register IS
+	PORT (
+		d: IN INTEGER;
+		load: IN STD_LOGIC;
+		
+		q: OUT INTEGER
+	);
+END integer_register;
+
+ARCHITECTURE behavior OF integer_register is
+BEGIN
+	PROCESS(load)
+	BEGIN
+		IF load='1' THEN
+			q <= d;
+		END IF;
+	END PROCESS;
+END behavior;
