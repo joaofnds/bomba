78a6d89b4474174b78993ea4b0eabab43e9365ce
João Fernandes <joaovitorfernandes2@gmail.com>
2018-07-04 01:48:25 -0300

    Add load and preset to regressive_counter

--- regressive_counter.vhd
+++ regressive_counter.vhd
@@ -6,22 +6,27 @@ USE ieee.numeric_std.all;
 ENTITY regressive_counter IS
 	PORT (
 		clock: IN STD_LOGIC;
+		load: IN STD_LOGIC;
 		reset: IN STD_LOGIC;
+		preset: IN INTEGER;
 		
 		q: OUT STD_LOGIC_VECTOR(5 DOWNTO 0)
 	);
 END regressive_counter;
 
 ARCHITECTURE behavior OF regressive_counter IS
-SIGNAL count: INTEGER RANGE 0 TO 59 := 59;
+constant COUNT_TO: INTEGER := 59; 
+SIGNAL count: INTEGER RANGE 0 TO COUNT_TO := COUNT_TO;
 BEGIN
 	PROCESS(clock, reset)
 	BEGIN
 		IF reset='1' THEN
 			count <= 0;
+		ELSIF load='1' THEN
+			count <= preset;
 		ELSIF rising_edge(clock) THEN
 			IF count=0 THEN
-				count <= 59;
+				count <= COUNT_TO;
 			ELSE
 				count <= count - 1;
 			END IF;
