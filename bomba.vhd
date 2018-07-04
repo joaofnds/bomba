f288c9179dcafd94eaf9deed510c88ea00ba49e6
João Fernandes <joaovitorfernandes2@gmail.com>
2018-07-04 05:16:41 -0300

    Update bomba architecture signals

--- bomba.vhd
+++ bomba.vhd
@@ -25,9 +25,20 @@ END bomba;
 
 ARCHITECTURE behavior OF bomba IS
 
-SIGNAL clock_segundos, clock_minutos, contagem_ativa: STD_LOGIC;
+SIGNAL clock_segundos,
+		 clock_minutos,
+		 display_mux_sel,
+		 contagem_ativa: STD_LOGIC;
+SIGNAL signal_generator_control: STD_LOGIC_VECTOR(1 DOWNTO 0);
 SIGNAL segundos_buffer, minutos_buffer: STD_LOGIC_VECTOR(5 DOWNTO 0);
-SIGNAL segundos, minutos, entrada: INTEGER;
+SIGNAL segundos,
+		minutos,
+		entrada,
+		display_buffer,
+		codigo_in,
+		cd_seg,
+		cd_min,
+		codigo: INTEGER;
 
 COMPONENT signal_generator
 	PORT(
@@ -89,34 +100,62 @@ END COMPONENT;
 
 BEGIN
 	gerador_de_sinal: signal_generator
-		PORT MAP (clock => clock, controle => control, q => clock_segundos);
+		PORT MAP (clock => CLOCK_50, controle => signal_generator_control, q => clock_segundos);
+		
+	cd_setter: countdown_setter
+		PORT MAP (keys => KEY, q_seg => cd_seg, q_min => cd_min);
 
 	segundos_counter: regressive_counter 
-		PORT MAP (clock => (clock_segundos AND contagem_ativa), reset => reset, q => segundos_buffer); 
+		PORT MAP (
+			clock => clock_segundos,
+			load => SW(3) AND NOT contagem_ativa,
+			preset => cd_seg,
+			reset => SW(2),
+			q => segundos_buffer
+		); 
    
-	clock_minutos <= segundos_buffer(5) AND 
+	clock_minutos <= segundos_buffer(5) AND
 						  segundos_buffer(4) AND 
-						  segundos_buffer(3) AND 
+				    	  segundos_buffer(3) AND 
 						  NOT segundos_buffer(2) AND 
-                    segundos_buffer(1) AND 
+						  segundos_buffer(1) AND 
 						  segundos_buffer(0); 
 
 	minutos_counter: regressive_counter 
-		PORT MAP (clock => clock_minutos, reset => reset, q => minutos_buffer);
+		PORT MAP (
+			clock => clock_minutos,
+			load => SW(3) AND NOT contagem_ativa,
+			preset => cd_min,
+			reset => SW(2),
+			q => minutos_buffer
+		);
 		
 	segundos <= to_integer(unsigned(segundos_buffer));
 	minutos <=  to_integer(unsigned(minutos_buffer));
 	entrada <= (minutos * 100) + segundos;
+	
+	codigo_in <= to_integer(unsigned(SW(17 DOWNTO 14)));
+	
+	codigo_register: integer_register
+		PORT MAP (d => codigo_in, load => SW(4), q => codigo);
+	
+	dp_mux: display_mux
+		PORT MAP (
+			data_0 => entrada,
+			data_1 => codigo,
+			sel => display_mux_sel,
+			q => display_buffer
+		);
 		
 	display_segundos: display
 		PORT MAP (
-			entrada => entrada,
-			display_0 => display0,
-			display_1 => display1,
-			display_2 => display2,
-			display_3 => display3,
-			display_4 => display4,
-			display_5 => display5
+			entrada => display_buffer,
+			display_0 => HEX0,
+			display_1 => HEX1,
+			display_2 => HEX2,
+			display_3 => HEX3,
+			display_4 => HEX4,
+			display_5 => HEX5
 		);
 
   contagem_segundos <= segundos_buffer;
